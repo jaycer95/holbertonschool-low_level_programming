68aa24a79102fb7da40f4ad164441a058f325f12
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-05 14:11:38 +0000

    Finish

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - prints to stderr
+ * @void: NULL
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 1
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
