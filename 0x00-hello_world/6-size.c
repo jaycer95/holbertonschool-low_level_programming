6b842d8131aac2881313369dc5b9994f8b96dc86
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-05 12:19:12 +0000

    Finish

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,18 @@
-#include<stdio.h>
-int main(void) {
-  printf("Size of a char: %lu byte(s)\n",sizeof(char));
-  printf("Size of an int: %lu byte(s)\n",sizeof(int));
-  printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
-  printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int));
-  printf("Size of a float: %lu bytes(s)\n",sizeof(float));
-  return (0);
+#include <stdio.h>
+/**
+ * main - main function
+ * @void: NULL
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n",sizeof(char));
+printf("Size of an int: %lu byte(s)\n",sizeof(int));
+printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n",sizeof(long long int));
+printf("Size of a float: %lu bytes(s)\n",sizeof(float));
+return (0);
 }
