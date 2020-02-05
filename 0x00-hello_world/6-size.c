9d4c674f80fe4ff602d61ff3a109bb2325155e92
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-05 12:57:58 +0000

    Finish

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 /**
- * main - main function
- * @void: NULL
-(* a blank line
-* Description: the first code executed when a program starts.)?
+ * main - prints a couple of lines
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
 (* section header: Section description)*
-* Return: Description of the returned value
+* Return: 0
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d bytes(s)\n", sizeof(float));
-return (0);
+  printf("Size of a char: %d byte(s)\n", sizeof(char));
+  printf("Size of an int: %d byte(s)\n", sizeof(int));
+  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+  printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+  printf("Size of a float: %d byte(s)\n", sizeof(float));
+  return (0);
 }
