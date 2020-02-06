79646d333b11f0cec73701efef2898c6bcafa8a0
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-06 16:57:05 +0000

    Fix

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,14 +9,14 @@
 */
 int main(void)
 {
-  int i, j, c;
-  for (i = 48; i <= 57; i++)
+int i, j, c;
+for (i = 48; i <= 57; i++)
 {
 for (j = 48; j <= 57; j++)
 {
 putchar(i);
 putchar(j);
-if (i != 57 && j != 57)
+if ((j != 57) || (i != 57))
 {
 for (c = 44 ; c >= 32 ; c -= 12)
 {
