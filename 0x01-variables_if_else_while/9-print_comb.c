15eb9332c443629afa953b69c6c15333636f0f74
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-06 15:09:28 +0000

    Finish

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,16 @@ int i, c;
 for (i = 48 ; i <= 57 ; i++)
 {
 putchar(i);
-for (c = 32 ; c >= 32 ; c+12)
+if (i != 57)
+{
+for (c = 44 ; c >= 32 ; c -= 12)
 {
 putchar(c);
+}
+}
+else
+{
+putchar('$');
 }
 }
 putchar('\n');
