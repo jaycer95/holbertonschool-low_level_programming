a50e610221e200010ed3d7dafb8db6d4692c63cc
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-06 12:39:43 +0000

    Advance

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints a couple of lines
+ * @void: Description of parameter x
+(*
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+for (i = 'z'; i = 'a' ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
