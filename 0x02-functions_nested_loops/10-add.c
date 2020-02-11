6a64d64d82d33d85162a829082fb09b913821459
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-11 08:46:16 +0000

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  * add - adds two variable.
- *
+ * @a: variable 1.
+ * @b: variable 2.
  * Return: sum.
  */
 int add(int a, int b)
 {
 int c;
 c = (a + b);
-return (c) ;
+return (c);
 }
