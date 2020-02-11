b82d381fcf6cd9d3ce735de88c18146d42f163ac
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-11 09:38:34 +0000

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,46 +6,44 @@
  */
 void jack_bauer(void)
 {
-int i,j,k,l;
+int i, j, k, l;
 for (i = 0; i <= 2; i++)
 {
-  if (i < 2)
-    {
+if (i < 2)
+{
 for (j = 0; j <= 9; j++)
 {
 for (k = 0; k <= 5; k++)
 {
 for (l = 0; l <= 9; l++)
 {
-_putchar(i+'0');
-_putchar(j+'0');
+_putchar(i + '0');
+_putchar(j + '0');
 _putchar(':');
-_putchar(k+'0');
-_putchar(l+'0');
+_putchar(k + '0');
+_putchar(l + '0');
 _putchar('\n');
- }
 }
 }
 }
-
+}
 else if (i == 2)
-   {  
+{
 for (j = 0; j <= 3; j++)
-     {
-       for (k = 0; k <= 5; k++)
-	 {
-	   for (l = 0; l <= 9; l++)
-	     {
-	       _putchar(i+'0');                                                                                                                                                        _putchar(j+'0');
-	       _putchar(':');
-	       _putchar(k+'0');
-	       _putchar(l+'0');
-	       _putchar('\n');
-	     }
+{
+for (k = 0; k <= 5; k++)
+{
+for (l = 0; l <= 9; l++)
+{
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(l + '0');
+_putchar('\n');
 }
 }
 }
 }
 }
-
-
+}
