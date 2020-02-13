455bdc6888eb9483d97b643ba2c72635d38c28ae
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-13 13:12:59 +0000

    test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - print numbers and words
@@ -10,24 +8,27 @@ int main(void)
 int i;
 for (i = 1; i = 100; i++)
 {
-if (i % 3 == 0)
+if ((i % 3) == 0 && (i % 5) == 0)
 {
-printf("Fuzz ");
+printf("FizzBuzz");
 }
-else if (i % 5 == 0)
+else if ((i % 3) == 0 && (i % 5) != 0)
 {
-if (i != 100)
-{
-printf("Buzz ");
+printf("Fizz");
 }
-else
+else if ((i % 3) != 0 && (i % 5) == 0)
 {
 printf("Buzz");
 }
-}
 else
 {
-printf("%i ", i);
+printf("%i", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
+printf("\n");
+return (0);
 }
