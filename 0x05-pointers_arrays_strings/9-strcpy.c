68c04797fc2b09da345da67beb9ce14c64d90ddf
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-17 20:35:39 +0000

    basic

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *_strcpy - print half last numbers.
+ * @*src: pointer.
+ * @*dest: pointer
+ * Return: char.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int n = 0, i; 
+while (src[n] != '\0')
+{
+n++;
+}
+for (i = 0; i <= n+1; i++)
+{
+dest[i] = src [i];
+}
+return dest;
+}
