c59125a9635a683f3a87b1061d8123c0c359e976
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-19 15:47:46 +0000

    push

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,19 +8,21 @@
  */
 int main(void)
 {
-int i = 0, n = 0, j = 0;
- char s[509];
-srand(time(0));
-while ( n <= 2772)
-{
-j = rand() % 127;
- if (j  > 32)     
-{ 
-s[i] = j ;
- i++;
-}
- printf("%d\n", s[i]);
- }
+  int i = 0, n = 0, j = 0;
+  char s[509];
+  srand(time(0));
+  while ( n <= 2772)
+    {
+      j = rand() % 127;
+      if (j  > 32)     
+	{ 
+	  s[i] = j ;
+	  i++;
+	}
+      printf("%d\n", s[i]); 
 
-return (0);
+      n += s[i];
+    }
+  printf("%i",n);
+  return (0);
 }
