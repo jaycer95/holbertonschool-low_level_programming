1985311134f0fdf32c67424a05206a88d081344c
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-20 22:53:33 +0000

    wrong

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * *rot13 - convert string to integer.
+ * @*s: pointer.
+ * Return: int.
+ */
+char *rot13(char *s)
+{
+int i = 0, j = 0;
+char a[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char m[]="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (s[i] != '\0')
+{
+for (j = 0; j < 50; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = m [j];
+}
+}
+i++;
+}
+return (s);
+}
