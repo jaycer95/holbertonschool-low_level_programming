f17d7770c81f4efc2aa9b412a577acc32efeb578
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-20 22:56:40 +0000

    wrong

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,15 +7,15 @@
 char *rot13(char *s)
 {
 int i = 0, j = 0;
-char a[]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char m[]="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char m[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 while (s[i] != '\0')
 {
 for (j = 0; j < 50; j++)
 {
 if (s[i] == a[j])
 {
-s[i] = m [j];
+s[i] = m[j];
 }
 }
 i++;
