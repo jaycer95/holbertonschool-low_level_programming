aff9339e4240dd929e66459d4d87245f46bf4627
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-24 11:24:06 +0000

    check

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * *_strcat - concats two string.
+ * @dest: destination.
+ * @src: source
+ * @n: unsigned integer.
+ * Return: dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0 ; i < n; i++)
+dest[i] = src[i];
+return (dest);
+}
