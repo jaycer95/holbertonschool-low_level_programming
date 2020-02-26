9462e171a1c36494b7f878245360ccd0bd0880a7
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-26 22:47:31 +0000

    finish

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -5,7 +5,7 @@
  *@s2: string
  * Return: 1 or 0.
  */
-int wildcmp(char *s1,char *s2)
+int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
@@ -15,5 +15,5 @@ if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 return (0);
 if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-return(0);
+return (0);
 }
