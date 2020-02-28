afa862f29067a88d0497c4f0c0bdb5ca585df1f2
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-28 12:00:09 +0000

    First try

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _abs - absolute value of n.
+ * @n: integer
+ * Return: positive n.
+ */
+int _abs(int n)
+{
+  if (n >= 0)
+    return (n);
+  else
+    return (-n);
+}
