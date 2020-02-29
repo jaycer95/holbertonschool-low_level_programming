9e84da6c44394f66f6196a0b69e09ecba646394f
jaycer95 <jacer.dabbabi95@gmail.com>
2020-02-29 12:15:09 +0000

    check

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * *_strcat - concats two string.
+ * @dest: destination.
+ * @src: source
+ * Return: dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i = 0, j = 0, k;
+  while (dest[i] != '\0')
+    {
+      i++;
+    }
+  while (src[j] != '\0')
+    {
+      j++;
+    }
+  for (k = 0 ; k <= j; k++)
+    {
+      dest[i + k] = src[k];
+    }
+  return (dest);
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strchr - locate a character in a string.
+ * @s: char.
+ * @c: char
+ * Return: dest.
+ */
+char *_strchr(char *s, char c)
+{
+  while (*s++)
+    {
+      if (*s == c)
+	{
+	  return (s);
+	}
+    }
+  return (0);
+}
