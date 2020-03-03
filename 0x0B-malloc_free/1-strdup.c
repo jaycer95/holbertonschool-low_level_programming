cd6215df61e254d50f458666188275f597a5593b
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-03 12:51:06 +0000

    ceck

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *_strcpy - print half last numbers.
+ * @src: pointer.
+ * @dest: pointer
+ * Return: char.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int n = 0, i;
+while (src[n] != '\0')
+{
+n++;
+}
+for (i = 0; i <= n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
+/**
+ * _strlen_recursion - calculate string length
+ * @s: string.
+ * Return: integer.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
+/**
+ * *_strdup - duplicate string
+ * @str: given string.
+ * Return: new string.
+ */
+char *_strdup(char *str)
+{
+char *dup;
+int i = _strlen_recursion(str);
+if (str != NULL)
+{
+dup = malloc(sizeof(char) * i);
+if (dup == NULL)
+{
+return (NULL);
+}
+else
+{
+return ((char *)_strcpy(dup, str));
+}
+}
+else
+{
+return (NULL);
+}
+}
