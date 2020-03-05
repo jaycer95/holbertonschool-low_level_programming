12a50bd514aa4fffa44fa5cb94b52ce28bec891e
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-05 13:45:43 +0000

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,10 +9,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 p = malloc(size * nmemb);
 if (p == NULL)
 return (NULL);
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
 return (p);
 }
