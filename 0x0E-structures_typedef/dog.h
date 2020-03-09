0d6b7cb995231a9301bf5e18970d4eb1a3405818
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-09 11:15:10 +0000

    push

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,17 @@
 #define _HOLBERTON_H_
 
 int _putchar(char c);
-struct dog {
-char * name;
+/**
+ * struct dog - structure.
+ * @name: char.
+ * @age: float.
+ * @owner: char.
+ */
+struct dog
+{
+char *name;
 float age;
-char * owner;
+char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
