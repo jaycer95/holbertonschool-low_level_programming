688e3f7a73a07f3fe1cb5487f09e56a772d4d499
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-15 13:19:14 +0000

    finish

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,36 +1,81 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 /**
+ * print_char - print a character.
+ * @arg: argument
+ * Return: void
+ */
+void print_char(va_list arg)
+{
+printf("%c", va_arg(arg, int));
+}
+/**
+ * print_integer - print an integer.
+ * @arg: argument
+ * Return: void
+ */
+void print_integer(va_list arg)
+{
+printf("%i", va_arg(arg, int));
+}
+/**
+ * print_float - print a float.
  *
- *
- *
- *
- *
- *
+ * @arg: list of argements.
+ */
+void print_float(va_list arg)
+{
+  printf("%f", va_arg(arg, double));
+}
+/**
+ * print_string - print a string.
+ * @arg: argement
+ * Return: void
+ */
+void print_string(va_list arg)
+{
+char *p;
+p = va_arg(arg, char *);
+if (p == NULL)
+p = "(nil)";
+printf("%s", p);
+}
+/**
+ * print_all - prints all.
+ * @format : list of the format 
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-int i, j;
-char z;
-struct operation 
-{
-  char *k;
-  char *l;
-  char *m;
-  char *n;
-};
-struct operation form = {"c", "i", "f", "s"};
 va_list arg;
+int i, j;
+char *sep = "";
+type_t type[] = {
+{"c", print_char},
+{"i", print_integer},
+{"f", print_float},
+{"s", print_string},
+{NULL, NULL} };
 va_start(arg, format);
-z = va_arg(arg, int);
-while(i != '\0')
+j = 0;
+while (format != NULL && format[j] != '\0')
 {
-  while(j <= 3)
-    {
-      if (z == form[j])
-	printf("%(form[j])", z);
-      j++;
-    }
-  i++;
+i = 0;
+while (type[i].character != NULL)
+{
+if (format[j] == *(type[i].character))
+{
+printf("%s", sep);
+(type[i].print_func)(arg);
+sep = ", ";
+ break;
 }
+i++;
+}
+j++;
+}
+printf("\n");
+va_end(arg);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,14 @@
 #ifndef _VAR_H_
 #define _VAR_H_
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+typedef struct type
+{
+  char *character;
+  void (*print_func)(va_list);
+} type_t;
 #endif
