d502b3aaaebb3f1155b1ac488e749acb7d1d7bec
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-15 14:12:34 +0000

    finish

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,7 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 typedef struct type
 {
-  char *character;
-  void (*print_func)(va_list);
+char *character;
+void (*print_func)(va_list);
 } type_t;
 #endif
