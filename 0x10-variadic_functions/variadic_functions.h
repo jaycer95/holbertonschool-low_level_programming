0e55c6c19491b1b8118ca7b183e0485311d26ad1
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-18 09:17:24 +0000

    push

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,11 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct type - structure
+ * @character: the character
+ * @print_func: print function
+ */
 typedef struct type
 {
 char *character;
