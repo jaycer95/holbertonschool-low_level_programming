2b1b5aeab83f039907b5e8911f537efc90b1ed04
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-21 13:45:54 +0000

    Emacs

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,24 +10,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *list;
-char *s;
-unsigned int i;
-s = strdup(str);
-if (s == NULL)
-return (NULL);
-while (s)
-{
-i++;
-}
-list = malloc(sizeof(list_t));
-if (list == NULL)
-return (NULL);
-list->str = s;
-list->len = i;
-list->next = NULL;
-if (head != NULL)
-list->next = *head;
-*head = list;
-return (*head);
+  list_t *list;
+  char *s;
+  unsigned int i;
+  s = strdup(str);
+  if (s == NULL)
+    return (NULL);
+  while (s)
+    {
+      i++;
+    }
+  list = malloc(sizeof(list_t));
+  if (list == NULL)
+    return (NULL);
+  list->str = s;
+  list->len = i;
+  list->next = NULL;
+  if (head != NULL)
+    list->next = *head;
+  *head = list;
+  return (*head);
 }
