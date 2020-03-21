89c73da4b1b71a667e9170660b613b24b2b5770d
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-21 17:26:03 +0000

    Finish

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free list
+ * @head: head
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *list;
+
+	while (head != NULL)
+	{
+		list = head;
+		head = head->next;
+		free(list->str);
+		free(list);
+	}
+
+}
