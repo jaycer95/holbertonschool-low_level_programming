413258a9cb18780f38ddf564e6f38ae0ad469ec1
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-24 19:54:49 +0000

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_listint - sums list data
+ * @head: head
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *list = head;
+	int k;
+
+	if (list == NULL)
+		return (0);
+	while (list != NULL)
+	{
+		k += list->n;
+		list = list->next;
+	}
+	return(k);
+}
