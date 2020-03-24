f6bcb192354524f7aa2625b7a6c22554fe332a5d
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-24 20:16:29 +0000

    9th

---
+++ 0x0D-preprocessor/hague.c
@@ -0,0 +1,28 @@
+#defineDIT(
+#defineDAH)
+#define__DAH++
+#define DITDAH*
+#defineDAHDITfor
+#defineDIT_DAHmalloc
+#define DAH_DITgets
+#define_DAHDITchar
+_DAHDIT _DAH_[]="ETIANMSURWDKGOHVFaLaPJBXCYZQb54a3d2f16g7c8a90l?e'b.s;i,d:"
+  ;mainDITDAH{_DAHDIT
+       DITDAH_DIT,DITDAHDAH_,DITDAH DIT_,
+       DITDAH_DIT_,DITDAHDIT_DAH DIT
+       DAH,DITDAHDAH_DIT DITDAH;DAHDIT
+				  DIT _DIT=DIT_DAHDIT 81DAH,DIT_=_DIT
+				  __DAH;_DIT==DAH_DITDIT _DITDAH;__DIT
+								   DIT'\n'DAH DAHDAHDIT DITDAH_=_DIT;DITDAH
+												       DAH_;__DITDITDITDAH
+													      _DIT_?_DAH DITDITDAHDIT_ DAH:'?'DAH,__DIT
+													      DIT' 'DAH,DAH_ __DAHDAH DAHDITDIT
+													      DITDAHDIT_=2,_DIT_=_DAH_;DITDAH _DIT_&&DIT
+																	 DITDAH _DIT_!=DITDITDAH DAH_>='a'?DITDAH
+																	 DAH_&223:DITDAHDAH_ DAH DAH;DIT
+																				       DITDAHDIT_ DAH __DAH,_DIT___DAH DAH
+																				       DITDAH DIT_+=DIT DITDAH _DIT_>='a'?DITDAH _DIT_-'a':0
+																				       DAH;}_DAH DIT DIT_DAH{__DIT DIT
+																						DIT_>3?_DAHDIT DIT_>>1 DAH:'\0'DAH;return
+																										     DIT_&1?'-':'.';}__DIT DITDIT_ DAH _DAHDIT
+																										     DIT_;{DIT void DAH write DIT1,&DIT_,1 DAH;}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+/**
+ *  listint_len - returns the length of the list .
+ * @h : list.
+ * Return: length.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *list = h;
+	unsigned int i = 0;
+
+	while (list)
+	{
+		list = list->next;
+		i++;
+	}
+	return (i);
+}
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head : list.
+ * @idx : index where to insert the new element.
+ * @n : the value of the new element.
+ * Return: length of the list.
+ */
+
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *list = *head, *exist;
+	unsigned int i = 0;
+
+	exist = malloc(sizeof(listint_t));
+	if (!exist)
+		return (NULL);
+	exist->n = n;
+	exist->next = NULL;
+	if (idx == 0)
+	{
+		if (list != NULL)
+			exist->next = *head;
+		*head = exist;
+		return (exist);
+	}
+	if (list != NULL)
+	{
+		i = listint_len(head);
+		if (idx > i + 1)
+			return (NULL);
+		if (idx == i + 1)
+			list->next = exist;
+		else
+		{
+			list = *head;
+			i = 0;
+			while (i < idx - 1)
+			{
+				l = l->next;
+				i++;
+			}
+			exist->next = list->next;
+			list->next = exist;
+		}
+		return (exist);
+	}
+	free(exist);
+	return (NULL);
+}
