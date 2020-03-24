24d8fe263771cede323d4657cebe73a3e355887c
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-24 15:18:59 +0000

    Free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+B#include "lists.h"
+/**
+ * free_listint2- frees list
+ * @head: list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	if (head != NULL)
+	{
+		list = *head;
+		while (list != NULL)
+		{
+			list = *head;
+			*head = list->next;
+			free(list);
+		}
+	}
+}
