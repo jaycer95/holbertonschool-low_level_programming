4ab3ffbb31e9db5237a8ad2691119e7d92e9ec7a
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-31 20:27:16 +0000

    Second

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * create - createss a text file and prints it to standard output.
+ * @filename : file name
+ * @text_content: content of file
+ * Return: integer.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int rtrn;
+	int fd;
+	int size;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (0);
+	if (text_content == NULL)
+		return (1);
+	while (text_content[size])
+		size ++;
+	rtrn = write(fd, tex_content, size);
+	if (print == -1)
+		return (-1);
+	close(fd);
+	return (rtrn);
+}
