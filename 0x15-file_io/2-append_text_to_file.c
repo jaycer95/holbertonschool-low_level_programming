20ead7ea74801c2db3961a644c37d020a4cc5eef
jaycer95 <jacer.dabbabi95@gmail.com>
2020-03-31 20:49:16 +0000

    Third

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * append_text_to_file - appends text to file.
+ * @filename: file name
+ * @text_content: text to append
+ * Return: integer.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	while (text_content[size])
 		size++;
 	rtrn = write(fd, text_content, size);
