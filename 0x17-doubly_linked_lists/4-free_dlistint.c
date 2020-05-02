0d467b1fa94f3d4cd868f3feb2c58ed3a82c9da7
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-02 23:23:59 +0000

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head: node
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *adv = head;
+	while(adv)
+	{
+		adv = head->next;
+		free(head);
+		head = adv;
+	}
+}
