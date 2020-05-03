22e5ff23d1ea657fb3c41bbcfd88e6a9a73f2613
jaycer95 <jacer.dabbabi95@gmail.com>
2020-05-03 01:22:48 +0000

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		adv = head->next;
+		adv = adv->next;
 		free(*head);
 		*head = adv;
 		if (adv != NULL)
